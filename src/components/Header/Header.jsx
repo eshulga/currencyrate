bf693828b04635fdff17c3f9a9ad18251ea8635e
Evgenii Shulga <ewgenij.shulga@gmail.com>
2019-02-27 00:19:01 +0200

    create structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+const Header = ({ classes }) => (
+  <AppBar className={classes.header}>
+    <h1>Currency</h1>
+  </AppBar>
+);
+
+export default Header;
