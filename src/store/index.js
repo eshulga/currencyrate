064a88a73f04266cfe8fd588ea1a0d767e0d910d
Evgenii Shulga <ewgenij.shulga@gmail.com>
2019-03-02 22:06:12 +0200

    ref: remove redux-debouncer

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import createDebounce from 'redux-debounced'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducers from '../reducers'
 
-const debouncer = createDebounce()
-
-const middleware = [debouncer, thunk]
+const middleware = [thunk]
 
 const enchancer = composeWithDevTools(applyMiddleware(...middleware))
 
